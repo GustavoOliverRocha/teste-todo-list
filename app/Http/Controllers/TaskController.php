f0559e90144e050fabf8742e1fc1eb134e304592
GustavoOliverRocha <gustaoliver18@gmail.com>
2022-07-29 01:16:36 -0300

    Implementacao dos Models,Controllers e seus metodos de CRUD e as Rotas

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $obj_task = new Task();
+        $obj_task->name = $request->taskName;
+        $obj_task->status = 'To do';
+        $obj_task->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $obj_task->name = $request->taskName;
+        $obj_task->status = $request->taskStatus;
+        $obj_task->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        if(isset($task->id))
+            $task->delete();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('todoList.pages.showAll');
 });
+
+/**
+ * TodoList Routes
+ * */
+Route::put('/list/update/{list_id}','App\Http\Controllers\TodoListController@update')
+->name('list.edit');
+
+Route::get('/list/{list_id}','App\Http\Controllers\TodoListController@showTasks')
+->name('list.showTasks');
+
+Route::delete('/list/delete/{list_id}','App\Http\Controllers\TodoListController@destroy')
+->name('list.delete');
+
+Route::post('/list/save','App\Http\Controllers\TodoListController@store')
+->name('list.save');
+
+Route::get('/list','App\Http\Controllers\TodoListController@show')
+->name('list.showAll');
+
+/**
+ * Tasks Routes
+ * */
+
+Route::delete('/task/delete/{list_id}','App\Http\Controllers\TaskController@destroy')
+->name('task.delete');
+
+Route::post('/task/save','App\Http\Controllers\TaskController@store')
+->name('task.save');
+
+
